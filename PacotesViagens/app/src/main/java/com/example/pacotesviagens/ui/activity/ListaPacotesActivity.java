1cae803bdfacf512cef7738961e1b555d43ecb5c
Gabriel <gnunes1909@gmail.com>
2022-03-07 17:05:11 -0300

    - Criação da tela de resumo do pacote. - Realizado o bind do pacote na tela resumo. - Implementando chains. - Refatorando todo o código java. - Refatorando todo o layout.

--- PacotesViagens/app/src/main/java/com/example/pacotesviagens/ui/activity/ListaPacotesActivity.java
+++ PacotesViagens/app/src/main/java/com/example/pacotesviagens/ui/activity/ListaPacotesActivity.java
@@ -2,6 +2,7 @@ package com.example.pacotesviagens.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
 
@@ -25,6 +26,8 @@ public class ListaPacotesActivity extends AppCompatActivity {
 
         //CTRL + ALT + M
         configuraLista();
+        Intent intent = new Intent(this, ResumoPacoteActivity.class);
+        startActivity(intent);
     }
 
     private void configuraLista() {
